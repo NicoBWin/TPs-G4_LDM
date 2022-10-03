345c4a9f7d3cd4c9f5a52b6ce9d88181b636af63
Nicolás <nbustelow@gmail.com>
2022-10-02 21:42:54 -0300

    minor update on MCP

--- TP2/TP2_G4/source/drivers/headers/MCP25625_driver.h
+++ TP2/TP2_G4/source/drivers/headers/MCP25625_driver.h
@@ -22,7 +22,7 @@
  * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
  ******************************************************************************/
 void MCP_init();
-void MCP_control(int instruction,int address,int txdata);
+void MCP_control(char instruction,char address,char txdata);
 void MCP_sendMSG(int txdata, _Bool ACSCONT);
 void MCP_endTX();
 
@@ -35,6 +35,7 @@ void MCP_endTX();
 #define MCP_INST_WRITE	0b00000010
 #define MCP_INST_BITMOD	0b00000101	//Bit modify
 #define MCP_INST_READST	0b00000010	//Read Status
+
 #define MCP_READRX_MASK	0b10010000
 #define MCP_READRX_NMSHIFT	2
 #define MCP_INST_READRX(x)             (((int)(((int)(x)) << MCP_READRX_NMSHIFT)) | MCP_READRX_MASK)
@@ -68,6 +69,72 @@ void MCP_endTX();
 #define MCP_CNF3_PHSEG2SHIFT	0
 #define MCP_CNF3_PHSEG2(x)             (((char)(((char)(x)) << MCP_CNF3_PHSEG2SHIFT)))
 
+#define MCP_TXRTSCTRL_ADDRESS 0x0D
+
+#define MCP_RXF0SIDH_ADDRESS 0x00
+#define MCP_RXF0SIDL_ADDRESS 0x01
+
+#define MCP_RXM0SIDH_ADDRESS 0x20
+#define MCP_RXM0SIDL_ADDRESS 0x21
+
+#define MCP_RXB0CTRL_ADDRESS		0x60
+#define MCP_RXB0CTRL_RXMSHIFT	5
+#define MCP_RXB0CTRL_RXM(x)             (((char)(((char)(x)) << MCP_RXB0CTRL_RXMSHIFT)))
+#define MCP_RXB0CTRL_BUKTSHIFT	2
+#define MCP_RXB0CTRL_BUKT(x)             (((char)(((char)(x)) << MCP_RXB0CTRL_BUKTSHIFT)))
+
+#define MCP_RXB1CTRL_ADDRESS		0x70
+#define MCP_RXB1CTRL_RXMSHIFT	5
+#define MCP_RXB1CTRL_RXM(x)             (((char)(((char)(x)) << MCP_RXB1CTRL_RXMSHIFT)))
+
+
+#define MCP_CANINTE_ADDRESS		0x2B
+#define MCP_CANINTE_MERRESHIFT	7
+#define MCP_CANINTE_MERRE(x)             (((char)(((char)(x)) << MCP_CANINTE_MERRESHIFT)))
+#define MCP_CANINTE_WAKIESHIFT	6
+#define MCP_CANINTE_WAKIE(x)             (((char)(((char)(x)) << MCP_CANINTE_WAKIESHIFT)))
+#define MCP_CANINTE_ERRIESHIFT	5
+#define MCP_CANINTE_ERRIE(x)             (((char)(((char)(x)) << MCP_CANINTE_ERRIESHIFT)))
+#define MCP_CANINTE_TX2IESHIFT	4
+#define MCP_CANINTE_TX2IE(x)             (((char)(((char)(x)) << MCP_CANINTE_TX2IESHIFT)))
+#define MCP_CANINTE_TX1IESHIFT	3
+#define MCP_CANINTE_TX1IE(x)             (((char)(((char)(x)) << MCP_CANINTE_TX1IESHIFT)))
+#define MCP_CANINTE_TX0IESHIFT	2
+#define MCP_CANINTE_TX0IE(x)             (((char)(((char)(x)) << MCP_CANINTE_TX0IESHIFT)))
+#define MCP_CANINTE_RX1IESHIFT	1
+#define MCP_CANINTE_RX1IE(x)             (((char)(((char)(x)) << MCP_CANINTE_RX1IESHIFT)))
+#define MCP_CANINTE_RX0IESHIFT	0
+#define MCP_CANINTE_RX0IE(x)             (((char)(((char)(x)) << MCP_CANINTE_RX0IESHIFT)))
+
+#define MCP_CANINTF_ADDRESS		0x2C
+#define MCP_CANINTF_MERRFSHIFT	7
+#define MCP_CANINTF_MERRF(x)             (((char)(((char)(x)) << MCP_CANINTF_MERRFSHIFT)))
+#define MCP_CANINTF_WAKIFSHIFT	6
+#define MCP_CANINTF_WAKIF(x)             (((char)(((char)(x)) << MCP_CANINTF_WAKIFSHIFT)))
+#define MCP_CANINTF_ERRIFSHIFT	5
+#define MCP_CANINTF_ERRIF(x)             (((char)(((char)(x)) << MCP_CANINTF_ERRIFSHIFT)))
+#define MCP_CANINTF_TX2IFSHIFT	4
+#define MCP_CANINTF_TX2IF(x)             (((char)(((char)(x)) << MCP_CANINTF_TX2IFSHIFT)))
+#define MCP_CANINTF_TX1IFSHIFT	3
+#define MCP_CANINTF_TX1IF(x)             (((char)(((char)(x)) << MCP_CANINTF_TX1IFSHIFT)))
+#define MCP_CANINTF_TX0IFSHIFT	2
+#define MCP_CANINTF_TX0IF(x)             (((char)(((char)(x)) << MCP_CANINTF_TX0IFSHIFT)))
+#define MCP_CANINTF_RX1IFSHIFT	1
+#define MCP_CANINTF_RX1IF(x)             (((char)(((char)(x)) << MCP_CANINTF_RX1IFSHIFT)))
+#define MCP_CANINTF_RX0IFSHIFT	0
+#define MCP_CANINTF_RX0IF(x)             (((char)(((char)(x)) << MCP_CANINTF_RX0IFSHIFT)))
+
+#define MCP_CANCTRL_ADDRESS		0x0F
+#define MCP_CANCTRL_REQOPSHIFT	5
+#define MCP_CANCTRL_REQOP(x)             (((char)(((char)(x)) << MCP_CANCTRL_REQOPSHIFT)))
+#define MCP_CANCTRL_ABATSHIFT	4
+#define MCP_CANCTRL_ABAT(x)             (((char)(((char)(x)) << MCP_CANCTRL_ABATSHIFT)))
+#define MCP_CANCTRL_OSMSHIFT	3
+#define MCP_CANCTRL_OSM(x)             (((char)(((char)(x)) << MCP_CANCTRL_OSMSHIFT)))
+#define MCP_CANCTRL_CLKENSHIFT	2
+#define MCP_CANCTRL_CLKEN(x)             (((char)(((char)(x)) << MCP_CANCTRL_CLKENSHIFT)))
+#define MCP_CANCTRL_CLKPRESHIFT	0
+#define MCP_CANCTRL_CLKPRE(x)             (((char)(((char)(x)) << MCP_CANCTRL_CLKPRESHIFT)))
 
 
 /*******************************************************************************
--- TP2/TP2_G4/source/drivers/src/MCP25625_driver.c
+++ TP2/TP2_G4/source/drivers/src/MCP25625_driver.c
@@ -9,8 +9,8 @@
  ******************************************************************************/
 
 // +Incluir el header propio (ej: #include "template.h")+
-#include "MCP25625_driver.h"
-#include "SPI_driver.h"
+#include "../headers/MCP25625_driver.h"
+#include "../headers/SPI_driver.h"
 #include "MK64F12.h"
 #include <stdbool.h>
 
@@ -48,44 +48,64 @@
  *******************************************************************************
  ******************************************************************************/
 void MCP_init(){
-SPI_t mySPI = {.mode=MASTER, .framesize=8,	.LSBFE=false, .CPHA=false,
-		.CKPOL=false, .CSPOL=true, .CONTCLK=false, .CONTCS=true, .CTARn=0};
-mySPI.PORT_settings.usePort=nPORTD;
-mySPI.PORT_settings.CS0pin=0;
-mySPI.PORT_settings.CLKpin=1;
-mySPI.PORT_settings.SOUTpin=2;
-mySPI.PORT_settings.SINpin=3;
+	SPI_t mySPI = {.mode=MASTER, .framesize=8,	.LSBFE=false, .CPHA=false,
+			.CKPOL=false, .CSPOL=true, .CONTCLK=false, .CONTCS=true, .CTARn=0};
+	mySPI.PORT_settings.usePort=nPORTD;
+	mySPI.PORT_settings.CS0pin=0;
+	mySPI.PORT_settings.CLKpin=1;
+	mySPI.PORT_settings.SOUTpin=2;
+	mySPI.PORT_settings.SINpin=3;
 
-//Definidos mis parametros,, llamo a SPI INIT
-SPI_MSTR_init(mySPI);
-//ACA defino la secuencia de inicializacion del MCP segun ppt
-// 1° RESETEO el controlador
-SPI_transfer_enqueue(MCP_INST_RESET, false);
-// 2° Configuro el tiempo de bit
-char CNF1=0,CNF2=0,CNF3=0;
-CNF1=(MCP_CNF1_SJW(0b01)|MCP_CNF1_BRP(0b10001));
-CNF2=(MCP_CNF2_BTL(1)|MCP_CNF2_SAM(1)|MCP_CNF2_PHSEG1(0b010)|MCP_CNF2_PRSEG2(0)|MCP_CNF2_PRSEG1(0)|MCP_CNF2_PRSEG0(0));
-CNF3=(MCP_CNF3_SOF(1)|MCP_CNF3_WAKFILL(1)|MCP_CNF3_PHSEG2(0b111));
-SPI_transfer_enqueue(MCP_INST_WRITE, true);
-SPI_transfer_enqueue(MCP_CNF1_ADDRESS, true);
-SPI_transfer_enqueue(CNF1, false);
-SPI_transfer_enqueue(MCP_INST_WRITE, true);
-SPI_transfer_enqueue(MCP_CNF2_ADDRESS, true);
-SPI_transfer_enqueue(CNF2, false);
-SPI_transfer_enqueue(MCP_INST_WRITE, true);
-SPI_transfer_enqueue(MCP_CNF3_ADDRESS, true);
-SPI_transfer_enqueue(CNF3, false);
+	//Definidos mis parametros,, llamo a SPI INIT
+	SPI_MSTR_init(mySPI);
+
+	//ACA defino la secuencia de inicializacion del MCP
+	// 1° RESETEO el controlador
+	SPI_transfer_enqueue(MCP_INST_RESET, false);
+
+	// 2° Configuro el tiempo de bit
+	char CNF1=0,CNF2=0,CNF3=0;
+	CNF1 = (MCP_CNF1_SJW(0b01)|MCP_CNF1_BRP(0b10001));
+	MCP_control(MCP_INST_WRITE, MCP_CNF1_ADDRESS, CNF1);
+
+	CNF2 = (MCP_CNF2_BTL(1)|MCP_CNF2_SAM(1)|MCP_CNF2_PHSEG1(0b010)|MCP_CNF2_PRSEG2(0)|MCP_CNF2_PRSEG1(0)|MCP_CNF2_PRSEG0(0));
+	MCP_control(MCP_INST_WRITE, MCP_CNF2_ADDRESS, CNF2);
+
+	CNF3 = (MCP_CNF3_SOF(1)|MCP_CNF3_WAKFILL(1)|MCP_CNF3_PHSEG2(0b111));
+	MCP_control(MCP_INST_WRITE, MCP_CNF3_ADDRESS, CNF3);
 
 
-// 3° Configuro los filtros de recepcion
+	// 3° Configuro los filtros de recepcion
+	//MCP_TXRTSCTRL_ADDRESS // Configurar para utilizar uno solo de los 3 buffers de TX
+// Aparte de configurar los filtros
+	/*
+	setear -> MCP_RXF0SIDH_ADDRESS, 0x20 // Seteo el filtro (H)
+	setear -> MCP_RXF0SIDL_ADDRESS, 0x00 // Seteo el filtro (L)
+	setear -> MCP_RXM0SIDH_ADDRESS, 0xFF // Seteo la mask (H)
+	setear -> MCP_RXM0SIDL_ADDRESS, 0x00 // Seteo la mask (L)
+	 */
 
-// 4° Configurar modo de recepción (RXB0CTRL y RXB1CTRL, chapter 4.2)
+	// 4° Configurar modo de recepción (RXB0CTRL y RXB1CTRL, chapter 4.2)
+	char RXB0CTRL=0, RXB1CTRL=0;
+	RXB0CTRL = (MCP_RXB0CTRL_RXM(0b00)|MCP_RXB0CTRL_BUKT(0));
+	MCP_control(MCP_INST_WRITE, MCP_RXB0CTRL_ADDRESS, RXB0CTRL);
+	RXB1CTRL = (MCP_RXB1CTRL_RXM(0b00));
+	MCP_control(MCP_INST_WRITE, MCP_RXB1CTRL_ADDRESS, RXB1CTRL);
 
-// 5° Borrar flags y habilitar interrupciones (CANINTF y CANINTE, chapter 4.7)
+	// 5° Borrar flags y habilitar interrupciones (CANINTF y CANINTE, chapter 4.7)
+	char CANINTF=0, CANINTE=0;
+	CANINTE = (MCP_CANINTE_MERRE(0)|MCP_CANINTE_WAKIE(0)|MCP_CANINTE_ERRIE(0)|MCP_CANINTE_TX2IE(0)
+			   |MCP_CANINTE_TX1IE(0)|MCP_CANINTE_TX0IE(0)|MCP_CANINTE_RX1IE(0)|MCP_CANINTE_RX0IE(0));
+	MCP_control(MCP_INST_WRITE, MCP_CANINTE_ADDRESS, CANINTE);
 
-// 6° Ponerlo en Normal Mode
-char CANCTRL=0;
+	CANINTF = (MCP_CANINTF_MERRF(0)|MCP_CANINTF_WAKIF(0)|MCP_CANINTF_ERRIF(0)|MCP_CANINTF_TX2IF(0)
+			   |MCP_CANINTF_TX1IF(0)|MCP_CANINTF_TX0IF(0)|MCP_CANINTF_RX1IF(0)|MCP_CANINTF_RX0IF(0));
+	MCP_control(MCP_INST_WRITE, MCP_CANINTF_ADDRESS, CANINTF);
 
+	// 6° Ponerlo en Normal Mode o Loopback
+	char CANCTRL=0;
+	CANCTRL = (MCP_CANCTRL_REQOP(0b000)|MCP_CANCTRL_ABAT(0)|MCP_CANCTRL_OSM(0)|MCP_CANCTRL_CLKEN(0)|MCP_CANCTRL_CLKPRE(0b00));
+	MCP_control(MCP_INST_WRITE, MCP_CANCTRL_ADDRESS, CANCTRL);
 }
 
 
@@ -94,11 +114,10 @@ void MCP_sendMSG(int txdata, _Bool ACSCONT){
 	SPI_transfer_enqueue(txdata, ACSCONT);
 }
 
-void MCP_control(int instruction,int address,int txdata){
-//	SPI_transfer_enqueue(instruction);
-//	SPI_transfer_enqueue(address);
-//	SPI_transfer_enqueue(txdata);
-//	SPI_PCS_dis();
+void MCP_control(char instruction,char address,char txdata){
+	SPI_transfer_enqueue(instruction, true);
+	SPI_transfer_enqueue(address, true);
+	SPI_transfer_enqueue(txdata, false);
 }
 
 void MCP_endTX(){
