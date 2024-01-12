d6b484fedbcb6aadf5b2f5fc1925860199ead98c
Nicolás <nbustelow@gmail.com>
2024-01-12 17:40:20 -0300

    Update DMA.c

--- TPF/TPF_G4/source/UI/Pdrivers/src/DMA.c
+++ TPF/TPF_G4/source/UI/Pdrivers/src/DMA.c
@@ -91,7 +91,7 @@ void DMA_Init(uint8_t DMA_channel, DMA_config_t config) {
 
 	//DMA0->TCD[DMA_channel].CSR &= ~DMA_CSR_INTMAJOR_MASK;
 	DMA0->TCD[DMA_0].CSR = DMA_CSR_INTMAJOR_MASK;	//Enable Major Interrupt.
-	DMA0->TCD[DMA_1].CSR &= ~(DMA_CSR_INTMAJOR_MASK);	//Enable Major Interrupt.
+	DMA0->TCD[DMA_1].CSR = DMA_CSR_INTMAJOR_MASK;	//Enable Major Interrupt.
 
 	/* Enable request signal. */
 	switch(DMA_channel) {
